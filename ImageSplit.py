1a4b069265bd4355b9b3e7b41960c152c1e11044
aturcitu <asbcn7@gmail.com>
2018-10-05 18:47:58 +0200

    hist added f

--- ImageSplit.py
+++ ImageSplit.py
@@ -4,10 +4,44 @@ Created on Thu Oct  4 19:52:08 2018
 
 @author: Aitor Sanchez
 """
-import cv2
 import numpy as np
+from scipy.stats import norm
 from matplotlib import pyplot as plt
-import ImageModel as imMod
 from ImageFeature import getGridOfImage 
 
-image_dict = getGridOfImage()
+
+def computeStats(image_dict):
+    #Stadistical study for the different signal types in order to properly
+    #split the training set into two sets,  ~70% and ~30% with the best 
+    #main features represented in both of them
+    fillRatio_dict = {}
+    formFactor_dict = {}
+    
+    for signalType in image_dict:
+        fillRatio_list = []
+        formFactor_list = []
+        for signalGrid in image_dict[signalType]:
+            fillRatio_list.append(signalGrid.fillRatio)
+            formFactor_list.append(signalGrid.formFactor)
+        fillRatio_dict[signalType] = fillRatio_list
+        formFactor_dict[signalType] = formFactor_list
+    
+    return (fillRatio_dict, formFactor_dict)
+        
+
+    
+
+if __name__ == '__main__':
+    imgType = 'C'        
+    try:
+        (fillRatio_dict, formFactor_dict) = computeStats(image_dict)   
+    except NameError:
+        image_dict = getGridOfImage()
+        (fillRatio_dict, formFactor_dict) = computeStats(image_dict)
+    
+    plt.hist(fillRatio_dict[imgType])
+    plt.ylabel('frequencia')
+    plt.xlabel('fillRatio')
+    plt.title('signalType '+imgType)
+    plt.show()
+
