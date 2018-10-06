52b7b48997254765ce531e6e927215f4d731271e
aturcitu <asbcn7@gmail.com>
2018-10-06 20:18:50 +0200

    Revert "Merge branch 'laura-dev' into aitor-dev"
    This reverts commit e82d3dac89aa9e68979f28eeda964cf8d0ee0657, reversing
    changes made to ddc4a122704358f6c2c5416c0d7a1054c45111c8.

--- ImageFeature.py
+++ ImageFeature.py
@@ -1,32 +1,64 @@
+__author__ = 'Zaius'
+
 import cv2
 import glob
 import numpy as np
 import os
 import ImageModel as imMod
-from matplotlib import pyplot as plt
-from create_dataframe import create_df
 from collections import defaultdict
+from matplotlib import pyplot as plt
 
 addPath = 'datasets/train/'
 addPathGt = 'datasets/train/gt/'
 addPathMask = 'datasets/train/mask/'
 
-# Dataframe
-df = create_df(addPath, addPathMask, addPathGt)
-
-def getPartialName(txtname):
-    pathList =txtname.split(".")
-    maskName = pathList[0] +"."+ pathList[1]
+mask_location_list = []
+mask_list = []
+        
+def getMaskFileName(txtname):
+    pathList = txtname.split(os.sep)
+    nameFileList = pathList[1].split(".")
+    maskName = addPathMask + "mask." + nameFileList[0] +"."+ nameFileList[1] + ".png"
     return maskName
 
-def getGridOfMask(imageName, i):
-    maskName=df["Mask"].iloc[i]
-    mask = cv2.imread(addPathMask+maskName,0)
-    area = mask[int(df["UpLeft(Y)"].iloc[i]):int(df["DownRight(Y)"].iloc[i]), int(df["UpLeft(X)"].iloc[i]):int(df["DownRight(X)"].iloc[i])]
-    
+def getGtFileName(txtname):
+    pathList = txtname.split(os.sep)
+    nameFileList = pathList[1].split(".")
+    maskName = addPathGt + "gt." + nameFileList[0] +"."+ nameFileList[1] + ".txt"
+    return maskName
+
+def getPartialName(txtname):
+    pathList = txtname.split(os.sep)
+    nameFileList = pathList[1].split(".")
+    maskName = nameFileList[0] +"."+ nameFileList[1]
+    return maskName
+
+def load_annotations(annot_file):
+    # Annotations are stored in text files containing
+    # the coordinates of the corners (top-left and bottom-right) of
+    # the bounding box plus an alfanumeric code indicating the signal type:
+    # tly, tlx, bry,brx, code
+    annotations = []
+
+    for line in open(annot_file).read().splitlines():
+
+        annot_values = line.split()
+        annot_values = [x.strip() for x in annot_values]
+        for ii in range(4):
+            annot_values[ii] = float(annot_values[ii])
+        annotations.append(annot_values)
+        
+    return annotations
+
+
+
+def getGridOfMask(imageName, values):
+          
+    maskName = getMaskFileName(imageName)
+    mask = cv2.imread(maskName,0)
+    area = mask[int(float(values[0])):int(float(values[2])), int(float(values[1])):int(float(values[3]))]
     fillRatioOnes = np.count_nonzero(area)
     sizeMatrix = np.shape(area)
-    
     fillRatioZeros = sizeMatrix[0]*sizeMatrix[1]
     fillRatio = fillRatioOnes/fillRatioZeros
     
@@ -34,36 +66,27 @@ def getGridOfMask(imageName, i):
         formFactor = abs(sizeMatrix[0]/sizeMatrix[1])
     else:
         formFactor = abs(sizeMatrix[1]/sizeMatrix[0])
-
+        
     return fillRatio, formFactor, area
-
+        
 def getGridOfImage():
     image_dict = defaultdict(list)
-    fillRatioL = []
-    formFactorL = []
+    for imageName in glob.glob(addPath+'*.jpg'):      
+        txtname = getGtFileName(imageName)
+        content = load_annotations(txtname)
+        for values in content: 
+            imageTrain = cv2.imread(imageName,1)
+            areaImg = imageTrain[int(float(values[0])):int(float(values[2])), int(float(values[1])):int(float(values[3]))]
+            fillRatio, formFactor, areaMask = getGridOfMask(imageName, values)
+            areaFinal = cv2.bitwise_and(areaImg,areaImg, mask = areaMask)           
+            #areaFinal = areaImg * areaMask
+            
+            partialName = getPartialName(imageName)
+            typeSignal = values[4].rstrip()
+            bean = imMod.ModelImage(areaImg, typeSignal, fillRatio, formFactor, partialName, areaMask, areaFinal)       
+            image_dict[typeSignal].append(bean)
     
-    for i in range(len(df)): 
-        imageName=df["Image"].iloc[i]
-        imageTrain = cv2.imread(addPath+imageName,1)
-        areaImg = imageTrain[int(df["UpLeft(Y)"].iloc[i]):int(df["DownRight(Y)"].iloc[i]), int(df["UpLeft(X)"].iloc[i]):int(df["DownRight(X)"].iloc[i])]
-        
-        fillRatio, formFactor, areaMask = getGridOfMask(imageName, i)
-        
-        areaFinal = cv2.bitwise_and(areaImg,areaImg,mask = areaMask) # Imagen final con la señal solo
-        partialName = getPartialName(imageName)
-        
-        typeSignal = df["Type"].iloc[i]
-        bean = imMod.ModelImage(areaImg, typeSignal, fillRatio, formFactor, partialName, areaMask, areaFinal)       
-        image_dict[typeSignal].append(bean)
-                
-        fillRatioL.append(fillRatio)
-        formFactorL.append(formFactor)
-       
-    df["FillRatio"]=fillRatioL
-    df["FormFactor"]=formFactorL
-        
-    return image_dict, df
-    
+    return image_dict
 
 def testMasks(img):
     testImg = img.imageGrid
@@ -73,16 +96,7 @@ def testMasks(img):
     plt.imshow(cv2.cvtColor(finalImg, cv2.COLOR_BGR2RGB))
     plt.show()
 
-def compute_histogram_type(signal_type):
-    hueL=[]
-    satL=[]
-    valL=[]
-    for i in range((len(image_dict[signal_type]))):
-        img = image_dict[signal_type][i]
-        testImg = img.finalGrid
-        cropimg = img.imageGrid
 
-<<<<<<< HEAD
 if __name__ == '__main__':
     imgType = 'B'
     try:
@@ -90,49 +104,7 @@ if __name__ == '__main__':
     except NameError:
         image_dict = getGridOfImage()
         testMasks(image_dict[imgType][0])
-=======
-        hsv = cv2.cvtColor(testImg, cv2.COLOR_BGR2HSV)
->>>>>>> laura-dev
-
-        hue, sat, val = hsv[:,:,0], hsv[:,:,1], hsv[:,:,2]
-
-        hueL.append(np.ndarray.flatten(hue))
-        satL.append(np.ndarray.flatten(sat))
-        valL.append(np.ndarray.flatten(val))    
-    
-    return hueL, satL, valL
 
 
-if __name__ == '__main__':
-#    imgType = 'C'
-#    try:
-#        testMasks(image_dict[imgType][0])    
-#    except NameError:
-#        image_dict = getGridOfImage()
-#        testMasks(image_dict[imgType][0])
 
-    image_dict, df = getGridOfImage()
-    df.to_csv('dataset.csv', encoding='utf-8', index=False)
-    hueTotal=[]
-    satTotal=[]
-    valTotal=[]
-    for file in df.Type.unique():
-        hue_type, sat_type, val_type = compute_histogram_type(file)
-        hueTotal.append(hue_type)
-        satTotal.append(sat_type)
-        valTotal.append(val_type)
-
-    # Show histograms
-    plt.figure(figsize=(10,8))
-    plt.subplot(311)                             
-    plt.subplots_adjust(hspace=.5)
-    plt.title("Hue")
-    plt.hist(hueTotal, bins=180)
-    plt.subplot(312)                             
-    plt.title("Saturation")
-    plt.hist(satTotal, bins=128)
-    plt.subplot(313)
-    plt.title("Luminosity Value")
-    plt.hist(valTotal, bins=128)
-    plt.show()
 
