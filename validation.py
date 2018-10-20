fix: validation computation

validation.py
@@ -98,18 +98,18 @@ def performance_accumulation_window(detections, annotations):
     
     TP = 0
     for ii in range (len(annotations)):
-        #for jj in range (len(detections)):
-        if (detections_used[ii] == 0) & (bbox_iou(annotations[ii], detections) > 0.5):
-            TP = TP+1
-            detections_used[ii]  = 1
-            annotations_used[ii] = 1
+        for jj in range (len(detections)):
+            if (detections_used[ii] == 0) & (bbox_iou(annotations[ii], detections[jj]) > 0.5):
+                TP = TP+1
+                detections_used[jj]  = 1
+                annotations_used[ii] = 1
 
     FN = np.sum(annotations_used==0)
     FP = np.sum(detections_used==0)
 
     return [TP,FN,FP]
 
-def performance_evaluation_window(TP, FN, FP):
+def performance_evaluation_window(TP, FP, FN):
     """
     performance_evaluation_window()
 
@@ -186,7 +186,7 @@ def validation_window(bboxes, pathToGT):
     TruePos = 0
     FalsePos = 0
     FalseNeg = 0
-
+    w = []
     for i in range(len(bboxes)):
         name, lista = bboxes[i]
         for x in lista:
@@ -194,16 +194,17 @@ def validation_window(bboxes, pathToGT):
             windows = x
             # In the first place of the list, the image name has to be stored so we can get the name
             gtFile = "gt."+name+".txt"
-    
+            
             annotations = load_annotations(pathToGT+'gt/'+gtFile)
-    
+            
+            windows = [windows]
             # remove image name to evaluate position
-            [pixelTP, pixelFP, pixelFN] = performance_accumulation_window(windows, annotations)
+            [pixelTP, pixelFN, pixelFP] = performance_accumulation_window(windows, annotations)
     
             TruePos += pixelTP
             FalsePos += pixelFP
             FalseNeg += pixelFN
-
+            
     [precision, sensitivity, accuracy] = performance_evaluation_window(TruePos, FalsePos, FalseNeg)
     
     print("\n------ FINAL RESULT Window Validation -------")