268bc449cf46aa5efa219b0111cc150439be8853
Laura <lmoraballestar@gmail.com>
2018-10-21 16:18:43 +0200

    add: test

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from validation import pixel_validation, validation_window
 import numpy as np
 from SlidingWindow import window_main
 from FastSlidingWindow import fast_sw
+import datetime
 
 #--->  KEY PATHS  <----#
 testPath = 'datasets/test/'
@@ -26,10 +27,15 @@ hsv_rang = (
      ,np.array([160,150,50]), np.array([180, 255, 255]) #DARK RED
      ,np.array([100,150,50]), np.array([140, 255, 255]) #BLUE
 )
-# ---> METHOD TO SAVE GT <--- #
-# el typeW indica la carpeta on guardaran els fitxers txt i les imatges
-typeW = "SLW_GT_RECT_IMPROV"
-method = 3
+
+# el typeW indicates where images and text files are going to be stored
+typeW = "SLW_GT_SQUARE_TEST"
+
+task = "SLW"
+
+if task == "SLW":
+    # set to 2 or 3
+    method = 1
 
 ###--->  DATA PARSING AND SPLIT  <----#
 if(LOAD_DATA == True):
@@ -47,14 +53,13 @@ if(LOAD_DATA == True):
 if(PLOT_STATS == True):
     plot_stats(df)
 #
-#if(USE_TRAIN == True):
-#    #--->  TRAIN DATA SEGMENTATION  <----#
-#    listOfBB = color_segmentation(dfTrain, trainSplitPath, hsv_rang)
-#    pixel_validation(dfTrain, trainSplitPath, 'colorMask')
-#    pixel_validation(dfTrain, trainSplitPath, 'morphologyMask')
-#    pixel_validation(dfTrain, trainSplitPath, 'finalMask')
-#    #validation_window(listOfBB, trainSplitPath)
-#    
+if(USE_TRAIN == True):
+    #--->  TRAIN DATA SEGMENTATION  <----#
+    listOfBB = color_segmentation(dfTrain, trainSplitPath, hsv_rang)
+    pixel_validation(dfTrain, trainSplitPath, 'colorMask')
+    pixel_validation(dfTrain, trainSplitPath, 'morphologyMask')
+    pixel_validation(dfTrain, trainSplitPath, 'finalMask')
+    
 if(USE_VALIDATION == True):
     #--->  VALIDATION DATA SEGMENTATION  <----#
     #listOfBB = color_segmentation(dfValidation, validationSplitPath, hsv_rang)
@@ -62,7 +67,7 @@ if(USE_VALIDATION == True):
     # amb el path, perteixes de gt o de la nostra segmentacio
     results = "resultMask/finalMask/"
     #results = "mask/"
-    import datetime
+    
     print ("Start: ", datetime.datetime.now())
     window_canditate =  window_main(dfValidation, validationSplitPath+results, dfStats, typeW, method)
     print ("End: ", datetime.datetime.now())
@@ -79,11 +84,48 @@ if(USE_VALIDATION == True):
     
     validation_window(dfValidation, validationSplitPath, typeW )
  
-#if(USE_TEST == True):
+if(USE_TEST == True):
+    
+    path_complete=testPath+"gtResult/"
+    
+    if task == "CCL":    
+        listOfBB = color_segmentation(dfTest, testPath, hsv_rang)
+    
+    elif task == "SLW":
+        
+        init = datetime.datetime.now()
+        
+        window_canditate =  window_main(dfTest, path_complete, dfStats, typeW, method)
+        
+        end = datetime.datetime.now()
+        print ("Total Time: ", end-init)
+        
+        # writing text files as result
+        for element in window_canditate:
+            name, positions = element
+            save_text_file(path_complete, positions, name, method, typeW)
+        
+        validation_window(dfTest, path_complete, typeW )
+      
+    
+    elif task == "SLW_FAST":
+        
+        init = datetime.datetime.now()
+        window_canditate =  fast_sw(dfValidation, validationSplitPath, dfStats)
+        
+        end = datetime.datetime.now()
+        print ("Total Time: ", end-init)
+        
+        # writing text files as result
+        for element in window_canditate:
+            name, positions = element
+            save_text_file(testPath, positions, name, method, typeW)
+            
+        validation_window(dfTest, path_complete, typeW )
+        
+    else:
+        print ("Entered method is invalid")
+    
     
 
-
-# SLIDING WINDOWS SPEED COMPARISON
-    
-#
     
