b9d605c04fddeaecbdf79413c4cb8ee0464a2e13
aturcitu <asbcn7@gmail.com>
2018-10-25 23:02:56 +0200

    cleaned for master

--- painting_retrieval/main.py
+++ painting_retrieval/main.py
@@ -1,6 +1,6 @@
 import cv2
 from matplotlib import pyplot as plt
-from utils import create_df, submission_list, save_pkl, mapk, get_image, plot_rgb
+from utils import create_df, submission_list, save_pkl, mapk, get_image, plot_rgb, plot_gray
 from method1 import store_histogram_total
 from task5 import haar_wavelet, haar_sticking
 import pandas as pd
@@ -15,33 +15,29 @@ k = 10
 # Read Images
 df = create_df(pathDS)
 # Save image descriptors
-hists= store_histogram_total(df, channel_name=['R','G','B'], level=1)
+store_histogram_total(df, channel_name=['R','G','B'], level=2)
 
+# --> MORE HERE <-- #
+# --> MORE HERE <-- #
+# --> MORE HERE <-- #
+# --> MORE HERE <-- #
 
-## Texture Descriptors - Haar Wavelets technique
-#imgTest = get_image(df.iloc[5], pathDS)
-#plt.imshow(imgTest)
-#plt.show()
-#grayImg = cv2.cvtColor(imgTest, cv2.COLOR_BGR2GRAY)
-#
-#level = 0
-#coeff = haar_wavelet(grayImg, level)
-#imgHaar = haar_sticking(coeff, level)
-#plt.imshow(imgHaar, cmap='gray')
-#plt.show()
+# Texture Descriptors - Haar Wavelets technique + GLCM
+imgTest = get_image(df.iloc[0]['Image'], pathDS)
+grayImg = cv2.cvtColor(imgTest, cv2.COLOR_BGR2GRAY)
+coeff = haar_wavelet(grayImg, level = 0)
+imgHaar = haar_sticking(coeff, level = 0)
+plot_gray(imgHaar)
 
-
-# Save Results..
-dfResult = pd.DataFrame({
+# Save and Evalaute Results..
+resultTest = pd.DataFrame({
     'Image' : ['im1', 'im3', 'im67', 'im97', 'im69', 'im46'],
     'Order' : [2, 1, 0, 1, 0, 2],
     'Query': [1, 1, 1, 2, 2, 2],
     })
 
-result_list = submission_list(dfResult)
+result_list = submission_list(resultTest)
 
 query_list = result_list
 evaluation = mapk(query_list, result_list, k)
-
-
 save_pkl(result_list, pathResults)
