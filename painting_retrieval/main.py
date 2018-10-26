1a8ad99dabacc328edafc86635adb36d055583c4
Laura <lmoraballestar@gmail.com>
2018-10-26 19:48:33 +0200

    testing

--- painting_retrieval/main.py
+++ painting_retrieval/main.py
@@ -5,12 +5,20 @@ from utils import create_df, get_image, plot_rgb
 from method1 import store_histogram_total
 from task1 import *
 
+import itertools
 # Paths
 pathDS = "dataset/"
 
 
 
-# Read Images
-df = create_df(pathDS)
-# Save image descriptors
-hists= store_histogram_total(df, channel_name=['R','G','B'], level=1)
+## Read Images
+#df = create_df(pathDS)
+### Save image descriptors
+#hists= store_histogram_total(df, channel_name=['R','G','B'], level=0)
+
+R = df["level0_R"].iloc[0][:]
+G = df["level0_G"].iloc[0][:]
+B = df["level0_B"].iloc[0][:]
+
+
+#print (list(itertools.chain.from_iterable(a)))
