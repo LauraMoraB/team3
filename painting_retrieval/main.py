9860f03fa0748b0ee247e60e3eb11701c8b3dad2
aturcitu <asbcn7@gmail.com>
2018-10-26 21:31:12 +0200

    multi cropping added to transform

--- painting_retrieval/main.py
+++ painting_retrieval/main.py
@@ -2,7 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 from utils import create_df, submission_list, save_pkl, mapk, get_image, plot_rgb, plot_gray
 from method1 import store_histogram_total
-from task5 import haar_wavelet, haar_sticking
+from task5 import haar_wavelet, haar_sticking, texture_region
 import pandas as pd
 
 # Paths
@@ -10,12 +10,12 @@ pathDS = "dataset/"
 pathQueries = "queries/"
 pathResults = "results/"
 
-# Number of results per query
-k = 10
-# Read Images
-df = create_df(pathDS)
-# Save image descriptors
-store_histogram_total(df, channel_name=['R','G','B'], level=2)
+## Number of results per query
+#k = 10
+## Read Images
+#df = create_df(pathDS)
+## Save image descriptors
+#store_histogram_total(df, channel_name=['R','G','B'], level=2)
 
 # --> MORE HERE <-- #
 # --> MORE HERE <-- #
@@ -23,21 +23,20 @@ store_histogram_total(df, channel_name=['R','G','B'], level=2)
 # --> MORE HERE <-- #
 
 # Texture Descriptors - Haar Wavelets technique + GLCM
-imgTest = get_image(df.iloc[0]['Image'], pathDS)
+imgTest = get_image(df.iloc[8]['Image'], pathDS)
 grayImg = cv2.cvtColor(imgTest, cv2.COLOR_BGR2GRAY)
-coeff = haar_wavelet(grayImg, level = 0)
-imgHaar = haar_sticking(coeff, level = 0)
-plot_gray(imgHaar)
+texture_region(grayImg, 0, 0)
 
-# Save and Evalaute Results..
-resultTest = pd.DataFrame({
-    'Image' : ['im1', 'im3', 'im67', 'im97', 'im69', 'im46'],
-    'Order' : [2, 1, 0, 1, 0, 2],
-    'Query': [1, 1, 1, 2, 2, 2],
-    })
+#coeff = haar_wavelet(grayImg, level = 0)
+#imgHaar = haar_sticking(coeff, level = 0)
+#plot_gray(imgHaar)
 
-result_list = submission_list(resultTest)
 
-query_list = result_list
-evaluation = mapk(query_list, result_list, k)
-save_pkl(result_list, pathResults)
+#
+## EVALUATION
+#resultList = []
+#queryList = []
+## MAPK RESULT
+#evaluation = mapk(queryList, resultList, k)
+## SAVE RESULTS INTO PKL FILE
+#save_pkl(resultList, pathResults)
--- painting_retrieval/task5.py
+++ painting_retrieval/task5.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pywt
 from skimage.feature import greycomatrix, greycoprops
+from task1 import compute_histogram
+from utils import plot_gray
 
 
 
@@ -23,10 +25,7 @@ def haar_wavelet(img, level = 0):
         return cA, cH, cV, cD
     else:
         coeffCA = haar_wavelet(cA, level -1)
-        coeffCH = haar_wavelet(cH, level -1)
-        coeffCV = haar_wavelet(cV, level -1)
-        coeffCD = haar_wavelet(cD, level -1)
-        return coeffCA, coeffCH, coeffCV, coeffCD
+        return coeffCA, cH, cV, cD
     
 # Stick together wavelet transform coefficient to form a new image of original size    
 def haar_sticking(coeff, level = 0):
@@ -34,11 +33,8 @@ def haar_sticking(coeff, level = 0):
         return np.vstack((np.hstack((coeff[0],coeff[1])),np.hstack((coeff[2],coeff[3]))))       
     else:
         stickCA = haar_sticking(coeff[0], level - 1)
-        stickCH = haar_sticking(coeff[1], level - 1)
-        stickCV = haar_sticking(coeff[2], level - 1)
-        stickCD = haar_sticking(coeff[3], level - 1)
-        return np.vstack((np.hstack((stickCA, stickCH)),np.hstack((stickCV, stickCD))))      
-
+        stickCA = norm_im(stickCA, coeff[1])
+        return np.vstack((np.hstack((stickCA, coeff[1])),np.hstack((coeff[2],coeff[3]))))    
 # Computes Gray-Level Co-Ocurrence Matrix in four direction with 1 pixel distance
 def GLCM(img) :
     # for each image 4 GLCM are computed at 0, 45, 90, -45 and -135º 
@@ -49,4 +45,35 @@ def GLCM(img) :
     imgStats.append(np.mean(greycoprops(glcm, 'correlation'))) 
     imgStats.append(np.mean(greycoprops(glcm, 'homogeneity')))      
     return glcm, imgStats
+
+
+def texture_region(im, level_div = 0, level_wavelet = 0):
+    """
+    im: image
+    level: level of segmentation
     
+    return: list of texture descriptors for the different image regions   
+    """
+
+    div = 2**level_div
+       
+    h, w = im.shape[0] , im.shape[1]
+   
+    w_step = int(w/div)
+    h_step = int(h/div)
+    
+    return [compute_texture(im[y:y+h_step,x:x+w_step], level_wavelet) \
+    for x in range(0,w,w_step) \
+        if x+w_step <= w \
+    for y in range(0,h,h_step) \
+        if y+h_step<= h]
+
+def compute_texture(im, level):
+    coeff = haar_wavelet(im, level)
+    imgHaar = haar_sticking(coeff, level)
+    plot_gray(imgHaar)
+    return None
+
+def norm_im(im, reference):
+    h, w = reference.shape[0] , reference.shape[1]
+    return im[:h,:w]
