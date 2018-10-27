1f672c0b89023de0d967652eea7b82c3d7e38b3c
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2018-10-27 16:52:52 +0200

    Fix task 3 output

--- painting_retrieval/task3.py
+++ painting_retrieval/task3.py
@@ -5,34 +5,14 @@ import matplotlib.pyplot as plt
 
 
 
-def getHistograms(image):
-    greyImage = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
-    equalized = cv2.equalizeHist(greyImage )
-    equalized_hist = cv2.calcHist([equalized],[0],None,[256],[0,256])
-    
-    blue_histr = cv2.calcHist([image],[0],None,[256],[0,256])
-    green_histr = cv2.calcHist([image],[1],None,[256],[0,256])
-    red_histr = cv2.calcHist([image],[2],None,[256],[0,256])
-    
-    return equalized_hist, blue_histr, green_histr, red_histr
-
-
-def getX2results(pathDataset, sourceImageName):
+def getX2results(histogram_list_dataset, histogram_query, K):
     distanceList = []
-    sourceImage = cv2.imread(pathDataset + sourceImageName, 1)
-    equalized_hist_source, blue_histr_source, green_histr_source, red_histr_source = getHistograms(sourceImage)
-
-    targetDf = create_df(pathDataset)
-    for i in range(len(targetDf)): 
-        dfSingle = targetDf.iloc[i]
-        targetImage = get_image(dfSingle, pathDataset)
-        equalized_hist_target, blue_histr_target, green_histr_target, red_histr_target = getHistograms(targetImage)
-        
-        distance = x2Distance(equalized_hist_target, equalized_hist_source)
-        distanceList.append(distance)
-        
-    distanceList.sort()
     
+    for histogram_dataset in histogram_list_dataset: 
+        distance = x2Distance(histogram_dataset, histogram_query)
+        distanceList.append(distance)
+    distanceList.sort()
+    distanceList =  distanceList[:K]
     return distanceList
         
 def getHellingerKernelResult(pathDataset, sourceImageName):
@@ -66,10 +46,3 @@ def getHistInterseccionResult(pathDataset, sourceImageName):
         intersectionList.append(intersection)
     intersectionList.sort()
     return intersectionList
-
-        
-pathDataset = "datasets/museum_set_random/"
-soruceImageName = "/ima_000000.jpg"
-distanceList = getX2results(pathDataset, soruceImageName)
-similarityList = getHellingerKernelResult(pathDataset, soruceImageName)
-intersectionList = getHistInterseccionResult(pathDataset, soruceImageName)
